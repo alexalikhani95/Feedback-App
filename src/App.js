20afc2ee62b3077ae9b78cc0a726bd6fc8516ff3
Alex Alikhani <alikhanialex@gmail.com>
2022-03-01 20:40:46 +0000

    Adding in header component and giving it default props/propTypes for text, bgcolor, textColor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <>
+    <Header text="Feedback UI"/>
+    <div className="container">
+      My app
     </div>
+    </>
   );
 }
 
