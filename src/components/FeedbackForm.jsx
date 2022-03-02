d260ad6c3c2f4e417614c903491013e070a49dff
Alex Alikhani <alikhanialex@gmail.com>
2022-03-02 21:38:08 +0000

    Adding in real time validation within the feedback form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,11 +2,23 @@ import React, { useState } from "react";
 import Button from "./shared/Button";
 import Card from "./shared/Card";
 
-
 const FeedbackForm = () => {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === '') { // If not text, set the button to be disabled
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if(text !== '' && text.trim().length <= 10) { // .trim trims away any whitespace
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+
     setText(e.target.value);
   };
 
@@ -16,9 +28,18 @@ const FeedbackForm = () => {
         <h2>How would you rate your service with us?</h2>
         {/* // To do - rating select component */}
         <div className="input-group">
-          <input type="text" placeholder="Write a review" onChange={handleTextChange} value={text}/>
-          <Button type="submit">Send</Button>
+          <input
+            type="text"
+            placeholder="Write a review"
+            onChange={handleTextChange}
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
