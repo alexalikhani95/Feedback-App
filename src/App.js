e1cf1ee7fb1f5097c0bad319b4323c5a1cab25ce
Alex Alikhani <alikhanialex@gmail.com>
2022-03-04 19:37:06 +0000

    Adding in AboutPage component and also react router in app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import { v4 as uuidv4 } from "uuid";
+import AboutPage from "./components/pages/AboutPage";
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
 
   const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback,...feedback]) // feedback to an array with all current feedback items and also the new one
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]); // feedback to an array with all current feedback items and also the new one
     console.log(newFeedback);
   };
 
@@ -24,14 +26,24 @@ const App = () => {
   };
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 };
 
